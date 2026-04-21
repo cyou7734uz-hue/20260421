89559fd685b367cab1e1bcedf069fb489a2ba62b
cyou7734uz-hue <cyou7734uz@gmail.com>
2026-04-21 10:40:39 +0800

    1.02產生一張圖片與視訊內容一樣的

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let capture;
+let pg;
 
 function setup() {
   // 建立全螢幕畫布
@@ -9,6 +10,10 @@ function setup() {
   
   // 隱藏預設出現在畫布下方的 HTML 影片元素，只在畫布內繪製
   capture.hide();
+
+  // 建立一個與預設視訊解析度相近的繪圖圖層
+  // 註：通常攝影機啟動需時間，這裡先建立一個基礎大小，draw 中會依需求縮放
+  pg = createGraphics(640, 480);
 }
 
 function draw() {
@@ -29,6 +34,21 @@ function draw() {
   translate(width, 0);
   scale(-1, 1);
   image(capture, x, y, videoWidth, videoHeight);
+
+  // 更新 Graphics 內容 (範例：在圖層中間畫一個圓與文字)
+  pg.clear(); // 確保背景透明
+  pg.stroke(255);
+  pg.strokeWeight(4);
+  pg.noFill();
+  pg.ellipse(pg.width / 2, pg.height / 2, 100, 100); 
+  pg.fill(255);
+  pg.noStroke();
+  pg.textSize(32);
+  pg.textAlign(CENTER, CENTER);
+  pg.text("Overlay Layer", pg.width / 2, pg.height / 2 + 80);
+
+  // 將 Graphics 內容繪製在視訊畫面之上，並縮放到與視訊相同的大小
+  image(pg, x, y, videoWidth, videoHeight);
   pop();
 }
 
